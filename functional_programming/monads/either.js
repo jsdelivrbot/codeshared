0a88346d45aed166d6f6c21d430aecf1c8be05d7
Son Dao <ngocson.dao@mobi.ch>
2017-08-03 17:59:31 +0700

    functional programming async

--- functional_programming/monads/either.js
+++ functional_programming/monads/either.js
@@ -1,89 +1,107 @@
-class Either {
-    constructor(value) {
-        this._value = value;
-    }
-    get value() {
-        return this._value;
-    }
-    static left(a) {
-        return new Left(a);
-    }
+exports.Either = class Either {
+	constructor(value) {
+		this._value = value;
+	}
+	
+	get value() {
+		return this._value;
+	}
+	
+	static left(a) {
+		return new exports.Left(a);
+	}
+	
+	static right(a) {
+		return new exports.Right(a);
+	}
+	
+	static fromNullable(val) {
+		return val !== null && val !== undefined ? Either.right(val) : Either.left(val);
+	}
+	
+	static of(a){
+		return Either.right(a);
+	}
+};
 
-    static right(a) {
-        return new Right(a);
-    }
+exports.Left = class Left extends exports.Either {
+	
+	map(_) {		
+		return this; // noop
+	}
+	
+	get value() {
+		throw new TypeError("Can't extract the value of a Left(a).");
+	}
+	
+	getOrElse(other) {
+		return other;
+	}
+	
+	orElse(f) {
+		return f(this._value);
+	}
+	
+	chain(f) {
+		return this;
+	}
 
-    static fromNullable(val) {
-        return val !== null ? right(val) : left(val);
+	getOrElseThrow(a) {
+		throw new Error(a);
+	}
+	
+	filter(f) {
+		return this;
+	}
+	
+	get isRight() {
+		return false;
+	}
 
-    }
-    static of(a) {
-        return right(a);
-    }
-}
+	get isLeft() {
+		return true;
+	}
 
-class Left extends Either {
-    map(_) {
-        return this; // noop
-    }
-    get value() {
-        throw new TypeError('Can"t extract the value of a Left(a).');
+	toString() {
+		return `Either.Left(${this._value})`;
+	}
+};
 
-    }
+exports.Right = class Right extends exports.Either {
+	
+	map(f) {		
+		return exports.Either.of(f(this._value));
+	}
+	
+	getOrElse(other) {
+		return this._value;
+	}
+	
+	orElse() {
+		return this;
+	}
+	
+	chain(f) {		
+		return f(this._value);
+	}
+	
+	getOrElseThrow(_) {
+		return this._value;
+	}
+	
+	filter(f) {		
+		return exports.Either.fromNullable(f(this._value) ? this._value : null);
+	}
 
-    getOrElse(other) {
-        return other;
-    }
-    orElse(f) {
-        return f(this.value);
-    }
-    chain(f) {
-        return this;
-    }
-    getOrElseThrow(a) {
-        throw new Error(a);
-    }
-    filter(f) {
-        return this;
-    }
-    toString() {
-        return `Either.Left(${this.value})`;
-    }
-}
+	get isRight() {
+		return true;
+	}
 
-class Right extends Either {
-    map(f) {
-        return Either.of(f(this.value));
-
-    }
-
-    getOrElse(other) {
-        return this.value;
-    }
-
-    orElse() {
-        return this;
-    }
-    chain(f) {
-        return f(this.value);
-    }
-
-    getOrElseThrow(_) {
-        return this.value;
-    }
-
-    filter(f) {
-        return Either.fromNullable(f(this.value) ? this.value : null);
-    }
-    toString() {
-        return `Either.Right(${this.value})`;
-    }
-}
-
-const R = require('ramda');
-
-const validLength = R.curry((len, str) => str.length === len)
-
-const checkLengthSSN = function(ssn) {
-    return Either.fromNullable(ssn).filter(validLength(9)).getOrElseThrow(`Input: ${ssn} is not a valid SSN number`);
-}
+	get isLeft() {
+		return false;
+	}
+	
+	toString() {
+		return `Either.Right(${this._value})`;
+	}
+};
