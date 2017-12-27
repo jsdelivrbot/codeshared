d2a8e9d7c990c9170e123892ec9f0c8548d9246a
Son Dao <ngocson.dao@mobi.ch>
2017-12-27 09:08:37 +0700

    additional code snippets

---
+++ RxJS/index.ts
@@ -0,0 +1,22 @@
+import { Observable, Observer } from 'rxjs';
+
+// const numbers: Observable<any> = Observable.of(1, 2, 3, 4, 5)
+//                     .switchMap((data) => number1(data));
+//
+// const number1 = (data: any) => {
+//     return Observable.create((observer: any) => {
+//         observer.next(data + 10);
+//         observer.complete();
+//     });
+// }
+//
+// console.log(`numbers = ${Object.prototype.toString.apply(numbers)}`);
+//
+// numbers.subscribe(
+//     (data) => {
+//         console.log(`data = ${data}`)
+//     }
+// );
+
+
+console.log("hello");
