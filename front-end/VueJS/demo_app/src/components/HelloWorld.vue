54b9bf58578ad5a4a2f71ba84374a5c7a81651c6
Danny Son Dao <sondnvn@gmail.com>
2018-10-01 23:33:11 +0700

    iterator, filter

--- front-end/VueJS/demo_app/src/components/HelloWorld.vue
+++ front-end/VueJS/demo_app/src/components/HelloWorld.vue
@@ -2,8 +2,11 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h5>Hello {{title}}</h5>
-    <ul v-for='item in items' v-bind:key='item.age'>
-      {{item.name}}
+    <ul>
+      <li v-for='item in items' v-bind:key='item.age'>
+        <p>{{item.name | capitalize | bold}}</p>
+        <span>The age is: {{item.age}}</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -16,12 +19,24 @@ export default {
   },
   data: () => {
     return {
-      title: 'danny',
-      items: [
-        { name:'danny', age: 33 },
-        { name: 'ryan', age: 29 },
-        { name: 'ruby', age: 28 }
-      ]
+      title: 'My App',
+      items: [{ name: 'danny', age: 33 }, { name: 'ryan', age: 29 }, { name: 'ruby', age: 28 }]
+    };
+  },
+  filters: {
+    capitalize: (value) => {
+      if (!value) {
+        return '';
+      }
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+    bold: (value) => {
+      if (!value) {
+        return '';
+      }
+      value = value.toString().bold();
+      return value;
     }
   },
   methods: {
@@ -29,7 +44,7 @@ export default {
       console.log('add item');
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -42,7 +57,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
